f3df6b35d2e485c938dd5d3c1a6f98287cbb2b18
Linda Gibson <dakidz@live.com>
2015-01-31 23:05:06 -0800

    add submit function for review form

--- js/app.js
+++ js/app.js
@@ -26,6 +26,10 @@
 
   app.controller("ReviewController", function(){
     this.review = {};
+
+    this.addReview = function(product) {
+      product.reviews.push(this.review);
+    };
   });
 
   var gems = [
